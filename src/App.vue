086cf24f781fb6944d5feec4a8a6cdfe18bac58f
ak-tr <62529128+ak-tr@users.noreply.github.com>
2022-11-28 21:01:57 +0000

    🎨 added zero padding to number and different coloured rings

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="container">
-    <Ring radius="360" :progress="month" stroke="50" />
-    <Ring radius="280" :progress="day" stroke="40" />
-    <Ring radius="210" :progress="hours" stroke="30" />
-    <Ring radius="150" :progress="minutes" stroke="20" />
-    <Ring radius="110" :progress="seconds" stroke="20" />
+    <Ring radius="360" :progress="month" stroke="50" strokeColour="#cdb4db" />
+    <Ring radius="280" :progress="day" stroke="40" strokeColour="#ffc8dd" />
+    <Ring radius="210" :progress="hours" stroke="30" strokeColour="#ffafcc" />
+    <Ring radius="150" :progress="minutes" stroke="20" strokeColour="#bde0fe" />
+    <Ring radius="115" :progress="seconds" stroke="20" strokeColour="#a2d2ff" />
   </div>
 </template>
 
@@ -32,17 +32,22 @@ export default {
     ) {
       return ((number - inMin) * (100 - 0)) / (inMax - inMin);
     },
+    zeroPad(number: number) {
+      // Pad single digit values with 0
+      return `0${Math.floor(number)}`.slice(-2);
+    }
   },
   mounted() {
     let dateObj = new Date();
 
     // Calculate last day of the month to
-    // calcualate correct scale for ring
+    // calculate correct scale for ring
     const lastDay = new Date(dateObj.getFullYear(), dateObj.getMonth() + 1, 0)
-    this.day = this.scale(dateObj.getDate(), 1, lastDay.getDate());
 
+    this.day = this.scale(dateObj.getDate(), 1, lastDay.getDate());
     this.month = this.scale(dateObj.getMonth() + 1, 1, 12);
 
+    // 1 second loop
     setInterval(() => {
       dateObj = new Date();
 
@@ -56,15 +61,16 @@ export default {
       // Map range of 0-60 to range 0-100
       const secondsScaled = this.scale(secondsWithMillis, 0, 60);
 
+      const minutesWithSeconds = parseFloat(`${minutes}.${this.zeroPad(secondsScaled)}`)
+      const minutesScaled = this.scale(minutesWithSeconds, 0, 61);
+
       this.seconds = secondsScaled;
+      this.minutes = minutesScaled;
       
-      // Only update minutes and hours when
-      // necessary
-      if (this.minutes != minutes)
-        this.minutes = minutes;
+      // Only render change in hours when necessary
       if (this.hours != hours)
         this.hours = this.scale(hours, 0, 24);
-    }, 500);
+    }, 1000);
   },
 };
 </script>
@@ -75,5 +81,11 @@ export default {
   display: flex;
   justify-content: center;
   align-items: center;
+  animation: entry 1s forwards cubic-bezier(.71,.1,.19,1.05);
+}
+
+@keyframes entry {
+  0% { transform: scale(0); }
+  100% { transform: scale(1); }
 }
 </style>
