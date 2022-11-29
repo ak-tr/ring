a2e7c14c4c3047949bb39e21a15b86946adeb774
ak-tr <62529128+ak-tr@users.noreply.github.com>
2022-11-29 01:42:03 +0000

    🎨 render by list of rings for dynamic sizing

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,21 @@
 <template>
   <div class="container">
-    <Ring radius="360" :progress="month" stroke="50" strokeColour="#cdb4db" />
-    <Ring radius="280" :progress="day" stroke="40" strokeColour="#ffc8dd" />
-    <Ring radius="210" :progress="hours" stroke="30" strokeColour="#ffafcc" />
-    <Ring radius="150" :progress="minutes" stroke="20" strokeColour="#bde0fe" />
-    <Ring radius="115" :progress="seconds" stroke="20" strokeColour="#a2d2ff" />
+    <Ring
+      v-for="ring in Object.values(rings)"
+      :key="ring.index"
+      :radius="ring.radius"
+      :progress="ring.progress"
+      :stroke="ring.stroke"
+      :strokeColour="ring.strokeColour"
+    />
   </div>
 </template>
 
 <script lang="ts">
 import Ring from "./components/Ring.vue";
+
+const width = window.innerWidth;
+const isSmall = width < 400;
 
 export default {
   components: {
@@ -17,35 +23,60 @@ export default {
   },
   data() {
     return {
-      seconds: 0,
-      minutes: 0,
-      hours: 0,
-      day: 0,
-      month: 0,
+      rings: {
+        month: {
+          index: 0,
+          radius: isSmall ? "220" : "360",
+          progress: 0,
+          stroke: isSmall ? 35 : 60,
+          strokeColour: "#cdb4db",
+        },
+        day: {
+          index: 1,
+          radius: isSmall ? "175" : "280",
+          progress: 0,
+          stroke: isSmall ? 30 : 50,
+          strokeColour: "#ffc8dd",
+        },
+        hours: {
+          index: 2,
+          radius: isSmall ? "135" : "210",
+          progress: 0,
+          stroke: isSmall ? 25 : 40,
+          strokeColour: "#ffafcc",
+        },
+        minutes: {
+          index: 3,
+          radius: isSmall ? "100" : "150",
+          progress: 0,
+          stroke: isSmall ? 20 : 30,
+          strokeColour: "#bde0fe",
+        },
+        seconds: {
+          index: 4,
+          radius: isSmall ? "70" : "115",
+          progress: 0,
+          stroke: isSmall ? 15 : 30,
+          strokeColour: "#a2d2ff",
+        },
+      },
     };
   },
   methods: {
-    scale(
-      number: number,
-      inMin: number,
-      inMax: number,
-    ) {
+    scale(number: number, inMin: number, inMax: number) {
       return ((number - inMin) * (100 - 0)) / (inMax - inMin);
     },
     zeroPad(number: number) {
       // Pad single digit values with 0
       return `0${Math.floor(number)}`.slice(-2);
-    }
+    },
   },
   mounted() {
     let dateObj = new Date();
 
     // Calculate last day of the month to
     // calculate correct scale for ring
-    const lastDay = new Date(dateObj.getFullYear(), dateObj.getMonth() + 1, 0)
-
-    this.day = this.scale(dateObj.getDate(), 1, lastDay.getDate());
-    this.month = this.scale(dateObj.getMonth() + 1, 1, 12);
+    const lastDay = new Date(dateObj.getFullYear(), dateObj.getMonth() + 1, 0);
 
     // 1 second loop
     setInterval(() => {
@@ -61,15 +92,23 @@ export default {
       // Map range of 0-60 to range 0-100
       const secondsScaled = this.scale(secondsWithMillis, 0, 60);
 
-      const minutesWithSeconds = parseFloat(`${minutes}.${this.zeroPad(secondsScaled)}`)
+      const minutesWithSeconds = parseFloat(
+        `${minutes}.${this.zeroPad(secondsScaled)}`
+      );
       const minutesScaled = this.scale(minutesWithSeconds, 0, 61);
 
-      this.seconds = secondsScaled;
-      this.minutes = minutesScaled;
-      
-      // Only render change in hours when necessary
-      if (this.hours != hours)
-        this.hours = this.scale(hours, 0, 24);
+      this.rings.seconds.progress = secondsScaled;
+      this.rings.minutes.progress = minutesScaled;
+
+      const day = this.scale(dateObj.getDate(), 1, lastDay.getDate());
+      const month = this.scale(dateObj.getMonth() + 1, 1, 12);
+
+      // Only render change in hours, days
+      // and months when necessary
+      if (this.rings.hours.progress != hours)
+        this.rings.hours.progress = this.scale(hours, 0, 24);
+      if (this.rings.day.progress != day) this.rings.day.progress = day;
+      if (this.rings.month.progress != month) this.rings.month.progress = month;
     }, 1000);
   },
 };
@@ -81,11 +120,16 @@ export default {
   display: flex;
   justify-content: center;
   align-items: center;
-  animation: entry 1s forwards cubic-bezier(.71,.1,.19,1.05);
+  animation: entry 1s forwards cubic-bezier(0.71, 0.1, 0.19, 1.05);
+  overflow: hidden;
 }
 
 @keyframes entry {
-  0% { transform: scale(0); }
-  100% { transform: scale(1); }
+  0% {
+    transform: scale(0);
+  }
+  100% {
+    transform: scale(1);
+  }
 }
 </style>
