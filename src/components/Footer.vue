62eef5aed42dc28c1fba27d069ea43a142618a0b
ak-tr <62529128+ak-tr@users.noreply.github.com>
2022-12-01 01:48:52 +0000

    💄 ui related footer changes

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="footer">
-    made with ♥ by akif - <a href="https://git.akif.kr/rings">github</a>
+    source code on <a href="https://git.akif.kr/rings">github</a><br>made with <span class="rotate">♥</span> by akif
   </div>
 </template>
 
@@ -19,15 +19,24 @@ export default {
   font-size: 12px;
   right: 0;
   bottom: 0;
-  padding: 15px;
+  padding: 0 10px 15px 0;
   color: grey;
   opacity: 0;
   animation: entry 1s linear 1s forwards;
   writing-mode: vertical-lr;
+  user-select: none;
+  line-height: 16px;
+  text-align: center;
 }
 
 a {
   color: grey;
+}
+
+.rotate {
+  display: inline-block;
+  transform: rotate(90deg);
+  -webkit-transform: rotate(90deg);
 }
 
 @keyframes entry {
